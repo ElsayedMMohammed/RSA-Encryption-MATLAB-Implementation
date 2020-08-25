1c096465c94fd2c846b9392c9cbb363a0afda548
ElSayedMMostafa <sayedmmostafa255@gmail.com>
2020-08-25 05:53:56 +0200

    Returns a coprime number with the input(Done)

--- get_coprime2.m
+++ get_coprime2.m
@@ -1,23 +1,40 @@
+%% This function returns one of the coprime for a number (k) which also less than k ang greater than 1
+
+
 function ss = get_coprime2(k)
 
- nums = 0;
+% Handle the exceptions
+ if k<2
+    disp('k must be greater than 1');
+    ss = -1;
+    return 
+ elseif k==2
+    ss = 1;
+    return 
+ end
+
+
+ nums = 0; % a vector hold all the tested numbers
  ss = 0; % a flag to check if no coprime number is found for k
-while length(nums)<k
+ 
+% Run a while loop to catch all the numbers (s), where 1 < s < k
+while length(nums)< k-1
     
 % get a random number between 1 & k
- s =fix(1+(k-1)*abs(rand(1)));
+ s =fix(2+(k-1)*abs(rand(1)));
  
 % To avoid repeated numbers
-  while ~is_new(nums,s)
-     s =fix(1+(k-1)*abs(rand(1)));
+  while ~is_new(nums,s) || s==k
+     s =fix(2+(k-1)*abs(rand(1)));
   end
   nums = [nums s];
  
 % Check the coprimality
-if get_gcd(k,s)==1 && s > 1
+if get_gcd(k,s)==1
     ss = s;
-    break;
+    return;
 end
 
- end
+end
+ disp('No coprime is found');
 end
