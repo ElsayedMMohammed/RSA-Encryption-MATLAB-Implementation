7546c68e31120d28c6231158ce0828d44dcf2a22
ElSayedMMostafa <sayedmmostafa255@gmail.com>
2020-08-25 05:56:26 +0200

    A simple function to check if a number exist in a vector or not(Done)

--- is_new.m
+++ is_new.m
@@ -1,12 +1,15 @@
+% A simple function to check if a number exist in a vector or not
+
 function a = is_new(MAT,num)
+
 len = length(MAT);
+a=1;
 
 for i=1:len
    if num==MAT(i)
        a = 0;
-      break; 
+       return; 
    end
-  a = 1;
 end
 
 end
