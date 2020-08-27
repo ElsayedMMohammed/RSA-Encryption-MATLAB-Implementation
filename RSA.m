b5aa032a54262e5340c42ec36ce6c84e111143dd
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2020-08-27 12:28:03 +0200

    Update RSA.m

--- RSA.m
+++ RSA.m
@@ -1,6 +1,6 @@
 %% The main script to apply RSA encryption algorithm and get the text
 clear; clc;
-bits = 13; %Specifiy the number of bits of the machine (MAX = 13)
+bits = 9; %Specifiy the number of bits of the machine (MAX = 13)
 
 %% (1) Get 2 distinct prime numbers between min, max.
   min = 2^(bits-1); max=2^bits;
@@ -18,22 +18,46 @@ bits = 13; %Specifiy the number of bits of the machine (MAX = 13)
   d = get_multi_inverse(e,k);
   
 %%  Encryption DEMO 
-  srcID = fopen('Input_Sample.txt','r'); %opens source txt
-  dstnID = fopen('Output_Sample.txt','wt'); %opens destination file
-  txt = fscanf(srcID,'%c'); %reads txt from source into matrix
-  txt = double(txt); %converts txt char into asci
+  srcID = fopen('Original_Input.txt','r'); %opens source txt
+  dstnID = fopen('Encrypted_Text.txt','wt'); %opens destination file
+  txt_string = fscanf(srcID,'%c'); %reads txt from source into matrix
+  txt = double(txt_string); %converts txt char into asci
   [r,c] = size(txt);
   nums = zeros(r,c);
+
   
   for i=1:r
       for j=1:c
          m = txt(i,j);
          c = mod_exponentiation2(m,e,n);
-         m2 = mod_exponentiation2(c,d,n);
-         nums(i,j)=m2;
+         %m2 = mod_exponentiation2(c,d,n);
+         nums(i,j)=c;
+         
       end
   end
   
-  fprintf(dstnID,'%s',nums); %writes txt to destination file (as string, so the ascii codes are converted to characters)
+  fprintf(dstnID,'%s',char(nums)); %writes txt to destination file (as string, so the ascii codes are converted to characters)
+  fclose(srcID);
+  fclose(dstnID);
+%% Decryption DEMO
+  srcID = fopen('Encrypted_Text.txt','r'); %opens source txt
+  dstnID = fopen('Decrypted_Text.txt','wt'); %opens destination file
+  txt_e_string = fscanf(srcID,'%c'); %reads txt from source into matrix
+  txt_e = double(txt_e_string); %converts txt char into asci
+  sum(txt_e == nums)/length(txt_e)
+  [r,c] = size(txt_e);
+  nums_d = zeros(r,c);
+  
+  for i=1:r
+      for j=1:c
+         m = txt_e(i,j);
+         m2 = mod_exponentiation2(m,d,n);
+         nums_d(i,j)=m2;
+        
+      end
+  end
+  sum(nums_d == txt)/length(txt)
+  
+  fprintf(dstnID,'%s',nums_d); %writes txt to destination file (as string, so the ascii codes are converted to characters)
   fclose(srcID);
   fclose(dstnID);
