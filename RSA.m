3e59c38bf4d9762c3287c8e61f8e89997d7e86fa
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2020-08-27 09:10:24 +0200

    Update RSA.m

--- RSA.m
+++ RSA.m
@@ -1,4 +1,4 @@
-%% The main script to apply RSA encryption algorithm
+%% The main script to apply RSA encryption algorithm and get the text
 clear; clc;
 bits = 13; %Specifiy the number of bits of the machine (MAX = 13)
 
@@ -18,8 +18,22 @@ bits = 13; %Specifiy the number of bits of the machine (MAX = 13)
   d = get_multi_inverse(e,k);
   
 %%  Encryption DEMO 
-  m = fix(1+26*abs(rand(1)));
-  c = mod_exponentiation2(m,e,n);
-  m2 = mod_exponentiation2(c,d,n);
+  srcID = fopen('Input_Sample.txt','r'); %opens source txt
+  dstnID = fopen('Output_Sample.txt','wt'); %opens destination file
+  txt = fscanf(srcID,'%c'); %reads txt from source into matrix
+  txt = double(txt); %converts txt char into asci
+  [r,c] = size(txt);
+  nums = zeros(r,c);
   
-  m==m2
+  for i=1:r
+      for j=1:c
+         m = txt(i,j);
+         c = mod_exponentiation2(m,e,n);
+         m2 = mod_exponentiation2(c,d,n);
+         nums(i,j)=m2;
+      end
+  end
+  
+  fprintf(dstnID,'%s',nums); %writes txt to destination file (as string, so the ascii codes are converted to characters)
+  fclose(srcID);
+  fclose(dstnID);
